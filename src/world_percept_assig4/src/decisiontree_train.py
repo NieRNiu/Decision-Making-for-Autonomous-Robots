d49888768ed9f80e869a1f31f522ac05b34d2893
Cecil Niu <141608325+KomaPlaton@users.noreply.github.com>
2025-01-13 10:46:49 +0100

    Update comments

--- src/world_percept_assig4/src/decisiontree_train.py
+++ src/world_percept_assig4/src/decisiontree_train.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 
-# 示例数据集，加入了性别和年龄
+# dataset
 data = {
     'taste_preference': ['bitter', 'sweet', 'neutral', 'bitter', 'sweet', 'neutral', 'sweet', 'bitter', 'neutral', 'sweet'],
     'prefers_alcohol': [True, True, False, True, False, True, True, False, True, False],
@@ -14,21 +14,20 @@ data = {
 
 df = pd.DataFrame(data)
 
-# 将分类数据转为数值数据
+# convert to label
 df['taste_preference'] = df['taste_preference'].map({'bitter': 0, 'sweet': 1, 'neutral': 2})
 df['prefers_alcohol'] = df['prefers_alcohol'].astype(int)
 df['budget'] = df['budget'].map({'low': 0, 'medium': 1, 'high': 2})
 df['gender'] = df['gender'].map({'male': 0, 'female': 1})
 df['selected_object'] = df['selected_object'].map({'beer': 0, 'coke': 1})
 
-# 特征和标签
 X = df[['taste_preference', 'prefers_alcohol', 'budget', 'gender', 'age']]
 y = df['selected_object']
 
-# 训练决策树模型
+# train the decision tree
 clf = DecisionTreeClassifier()
 clf.fit(X, y)
 
-# 保存模型
+# save model
 with open('decision_tree_model.pkl', 'wb') as model_file:
     pickle.dump(clf, model_file)
